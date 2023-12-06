6a5ea362666fde6a11045ce8b53b9a03b92160f9
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-12-07 02:52:28 +0900

    [FEAT]로그인창 구현

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
-import React from 'react'
+// PasswordInput.js
+
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+// 스타일을 정의한 컴포넌트
+const Container = styled.div`
+  margin: 20px;
+  display: flex;
+  gap : 10px;
+  flex-direction: column; /* 세로(column) 방향으로 변경 */
+  align-items: center; /* 가로 중앙 정렬 */
+  justify-content: center; /* 세로 중앙 정렬 */
+  text-align: center; /* 텍스트 중앙 정렬 */
+  height: 100vh; /* 뷰포트 높이에 맞게 설정 */
+  `;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 8px;
+  font-size: 16px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  font-size: 16px;
+`;
+
+const LoginButton = styled.button`
+  background-color: green;
+  color: white;
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+`;
+
+const ProfileImage = styled.img`
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  margin-right: 10px;
+`;
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleEnterPress = (e) => {
+    if (e.key === 'Enter') {
+      console.log('비밀번호:', password);
+    }
+  }; 
+  const handleLoginButtonClick = () => {
+    console.log('비밀번호:', password);
+  }; 
+  return (
+    <Container>
+      <ProfileImage src="logo192.png" alt="Profile" />
+      <div>
+        <Label htmlFor="password">비밀번호:</Label>
+        <Input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          onKeyDown={handleEnterPress}
+
+      />
+      </div>
+      <LoginButton onClick={handleLoginButtonClick}>로그인</LoginButton>
+
+    </Container>
+  );
+};
+
+export default Login;
